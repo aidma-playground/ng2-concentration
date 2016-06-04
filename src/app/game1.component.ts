1c1c64fa15250bd020b413678311856320c7a854
山崎裕基 <Yuki@yamazakiyuuki-no-MacBook-Pro.local>
2016-06-04 20:12:37 +0900

    first commit

---
+++ src/app/game1.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'game1-app',
+  templateUrl: 'game1.component.html',
+  styleUrls: ['game1.component.css']
+})
+export class Game1AppComponent {
+  title = 'game1 works!';
+}
